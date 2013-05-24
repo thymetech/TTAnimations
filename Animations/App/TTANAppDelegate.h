a6ea053e6632af1324f4518a6fcadb50c0aaf08c
Mason Weems <mason@thymetech.com>
2013-05-24 11:42:41 -0500

    Add storyboard for iPhone with navigation controller and table view

---
+++ Animations/App/TTANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTANAppDelegate.h
+//  Animations
+//
+//  Created by Mason Weems on 5/24/13.
+//  Copyright (c) 2013 Thyme Technology Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
